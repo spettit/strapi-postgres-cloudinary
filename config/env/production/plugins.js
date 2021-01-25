7ac87356b2044ddc3d15bb0a606adc45e463b06e
Sean Pettit <s.p@mac.com>
2021-01-25 18:44:14 +0000

    cloudinary default folder

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       cloud_name: env("CLOUDINARY_NAME"),
       api_key: env("CLOUDINARY_KEY"),
       api_secret: env("CLOUDINARY_SECRET"),
+      default_folder: "strapi"
     },
   },
 });
